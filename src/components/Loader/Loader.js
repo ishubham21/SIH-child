a03398948477348fcd8dee8c1f79cda39ef5d3a8
adayush <adayush0@gmail.com>
2022-08-26 18:37:43 +0530

    fixed navbar, added logo

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,8 +1,12 @@
 import style from "./Loader.module.css"
 
 const Loader = () => {
+  const css = `header {
+    display: none;
+  }`
   return (
     <div className={style.loaderContainer}>
+      <style>{css}</style>
       <div className={style.loader}></div>
     </div>
   )
